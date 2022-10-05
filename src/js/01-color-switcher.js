1ec172aded57795983d917da330b2e3c29d7c3ad
ChemerysIgor <chemeris_igor@ukr.net>
2022-10-05 16:50:32 +0300

    home-work 9
    create 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
 
+const start = document.querySelector(`button[data-start]`);
+const stop = document.querySelector(`button[data-stop]`);
+start.classList.add("active");
+let timerId = null;
+
+start.addEventListener(`click`, onStart);
+function onStart (event){
+  if (start.classList.contains("active"))
+  {   const body = document.querySelector(`body`);
+         const getRandomHexColor = () => {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      };
+      timerId = setInterval(() =>{body.style.backgroundColor = getRandomHexColor()},1000);
+      start.classList.remove("active");
+    } 
+ };
+    stop.addEventListener(`click`, onStop); 
+    function onStop (evt){      
+            clearInterval(timerId);
+            start.classList.add("active");}
